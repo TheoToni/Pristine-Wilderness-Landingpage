d6ec626d7dffaf15b7a1afffd960cadf733ecec2
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-04-22 20:56:40 +0200

    backdrop

--- pristinewilderness/src/app/page.tsx
+++ pristinewilderness/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#7ebead] m-20 shadow-2xl rounded-md">
-      <div className="wrapper flex min-h-screen">
+      <div className="wrapper flex min-h-screen ">
         <h1 className="text-white text-5xl font-bold">
           Experience the power of the nature
         </h1>
