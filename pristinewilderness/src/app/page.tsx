601b31a2eb56348583e7130c7274bd17ae7647ed
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-04-24 22:48:58 +0200

    progress

--- pristinewilderness/src/app/page.tsx
+++ pristinewilderness/src/app/page.tsx
@@ -2,20 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex justify-center h-screen  bg-gradient-to-r from-[#0F2459] to-[#E6820B]  ">
+    <main className="flex justify-center h-screen items-center  bg-gradient-to-r from-[#0F2459] to-[#E6820B]">
       <div className="wrapper flex flex-col items-center justify-center">
-        <h1 className="text-white text-5xl font-bold">
-          Experience the power of the nature
+        <h1 className="text-white z-10 uppercase text-5xl font-bold mb-20">
+          Explore the AI nature
         </h1>
-        <div className="flex justify-center items-center">
-          <Image
-            className=" rounded-xl"
-            src="/astronaut.jpg"
-            alt="astronaut in the jungle"
-            width={350}
-            height={100}
-          ></Image>
-        </div>
+
+        <Image
+          className=" rounded-xl z-2 absolute"
+          src="/astronaut.jpg"
+          alt="astronaut in the jungle"
+          width={350}
+          height={100}
+        ></Image>
+        <Image
+          className=" rounded-xl blur-sm absolute"
+          src="/astronaut.jpg"
+          alt="astronaut in the jungle"
+          width={360}
+          height={100}
+        ></Image>
       </div>
     </main>
   );
