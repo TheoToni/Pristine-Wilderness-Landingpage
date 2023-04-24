c0b4919f2a1f371f0f74ccaa879c9a0e83140686
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-04-24 23:08:45 +0200

    progress

--- pristinewilderness/src/app/page.tsx
+++ pristinewilderness/src/app/page.tsx
@@ -4,9 +4,12 @@ export default function Home() {
   return (
     <main className="flex justify-center h-screen items-center  bg-gradient-to-r from-[#0F2459] to-[#E6820B]">
       <div className="wrapper flex flex-col items-center justify-center">
-        <h1 className="text-white z-1  text-5xl font-bold mb-20 absolute top-10">
+        <h1 className="text-white z-1  text-3xl  mb-20 absolute top-10">
           Explore the AI nature
         </h1>
+        <p className="absolute bottom-10 text-white">
+          Library provided by THEO.JS
+        </p>
 
         <Image
           className=" rounded-xl z-10 absolute"
