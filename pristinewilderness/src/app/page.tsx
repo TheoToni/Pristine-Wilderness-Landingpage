dc89c1ab7ff5de099bd753f43eccfa3c7e562d3c
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-04-24 22:21:48 +0200

    Just trying something

--- pristinewilderness/src/app/page.tsx
+++ pristinewilderness/src/app/page.tsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#7ebead] m-20 shadow-2xl rounded-md">
-      <div className="wrapper flex min-h-screen ">
+    <main className="flex flex-row   bg-gradient-to-b from-slate-900 to-slate-700  ">
+      <div className="wrapper flex h-screen bg-  ">
         <h1 className="text-white text-5xl font-bold">
           Experience the power of the nature
         </h1>
+
+        <Image
+          className=" rounded-xl"
+          src="/astronaut.jpg"
+          alt="astronaut in the jungle"
+          width={600}
+          height={100}
+        ></Image>
       </div>
     </main>
   );
